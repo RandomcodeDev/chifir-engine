a17443471477c459a1291b2e260ea6a07b315cd4
MobSlicer152 <brambleclaw1414@gmail.com>
2024-06-04 08:06:38 -0400

    Add part of device manager

---
+++ render/impl/INVRHIDeviceManager.h
@@ -0,0 +1,20 @@
+/// @file INVRHIDeviceManager.h
+/// @brief This file defines the interface for API-specific functionality required by NVRHI.
+/// @copyright (c) 2024 Randomcode Developers. See LICENSE.
+
+#pragma once
+
+#include "framework/Framework.h"
+
+#include "nvrhi/nvrhi.h"
+
+class INVRHIDeviceManager
+{
+  public:
+	virtual ~INVRHIDeviceManager() = default;
+
+	/// @brief Create a nvrhi::DeviceHandle
+	virtual nvrhi::DeviceHandle CreateDevice() = 0;
+
+	/// @brief
+};
