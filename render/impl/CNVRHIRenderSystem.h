a2f7c35f51735d9bcbd8d2b5464a614b543236d6
MobSlicer152 <brambleclaw1414@gmail.com>
2024-06-06 07:43:15 -0400

    Add CNVRHIRenderSystem

---
+++ render/impl/CNVRHIRenderSystem.h
@@ -0,0 +1,18 @@
+/// @file CNVRHIRenderSystem.h
+/// @brief This file declares the NVRHI-based implementation of IRenderSystem
+/// @copyright (c) 2024 Randomcode Developers. See LICENSE.
+
+#pragma once
+
+#include "framework/Framework.h"
+#include "framework/Log.h"
+#include "framework/Util.h"
+
+#include "render/IRenderSystem.h"
+
+#include "INVRHIDeviceManager.h"
+
+class CNVRHIRenderSystem : public IRenderSystem
+{
+
+};
