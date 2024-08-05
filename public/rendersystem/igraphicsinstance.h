b8f1c34ed1213293401af2175c7ba719727c0dde
MobSlicer152 <brambleclaw1414@gmail.com>
2024-08-05 12:55:37 -0400

    Rename graphics stuff

--- public/rendersystem/igraphicsinstance.h
+++ public/rendersystem/igraphicsinstance.h
@@ -6,24 +6,24 @@
 #include "base/types.h"
 #include "base/vector.h"
 
-struct GraphicsDeviceInfo_t;
-class IGraphicsDevice;
-class IGraphicsSwapChain;
+struct RhiDeviceInfo_t;
+class IRhiDevice;
+class IRhiSwapChain;
 
 // Represents a DXGI factory, a VkInstance, or whatever ungodly thing legacy APIs use (GL context, etc)
-class IGraphicsInstance
+class IRhiInstance
 {
-	virtual ~IGraphicsInstance() DEFAULT;
+	virtual ~IRhiInstance() DEFAULT;
 
 	// Destroys the instance, all objects created off of it must be destroyed before this
 	virtual void Destroy() = 0;
 
 	// Get information about the graphics devices present
-	virtual CVector<GraphicsDeviceInfo_t> GetDeviceInfo() = 0;
+	virtual CVector<RhiDeviceInfo_t> GetDeviceInfo() = 0;
 
 	// Create a graphics device
-	virtual IGraphicsDevice* CreateDevice() = 0;
+	virtual IRhiDevice* CreateDevice() = 0;
 
 	// Create a swap chain
-	virtual IGraphicsSwapChain* CreateSwapChain() = 0;
+	virtual IRhiSwapChain* CreateSwapChain() = 0;
 };
