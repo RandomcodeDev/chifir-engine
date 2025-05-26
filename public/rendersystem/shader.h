1058e79b34dadbf689b88cd323bbe6d40b30de6e
MobSlicer152 <brambleclaw1414@gmail.com>
2025-05-26 14:45:53 -0400

    Start making interface for memory in the RHI, start adding future math headers

---
+++ public/rendersystem/shader.h
@@ -0,0 +1,9 @@
+/// \file Declarations for using shaders
+/// \copyright 2025 Randomcode Developers
+
+#pragma once
+
+#include "base/compiler.h"
+#include "base/types.h"
+
+
--- public/rhi/irhibuffer.h
+++ public/rhi/irhibuffer.h
@@ -6,12 +6,51 @@
 #include "base/compiler.h"
 #include "base/types.h"
 
-enum class RhiMemoryLocation
+#include "irhimemory.h"
+
+/// Represents the ways a buffer will be used. Some combinations aren't valid.
+enum class RhiBufferUsage
 {
-    Host,
-    Device,
-    Shared
+	TransferSrc,
+	TransferDest,
+	VertexBuffer,
+	IndexBuffer,
+	UniformBuffer,
+	IndirectBuffer,
+	// TODO: ray tracing stuff
 };
 
-enum class RhiMemoryUsage;
-class IRhiBuffer;
+/// Represents any memory buffer controller by the graphics API
+class IRhiBuffer
+{
+  public:
+	virtual ~IRhiBuffer() = default;
+
+	/// Get the memory that backs this buffer
+	virtual IRhiMemory* GetMemory() = 0;
+
+	/// Get the size of this buffer
+	virtual ssize GetSize() = 0;
+};
+
+/// A buffer that the host can see
+class IRhiHostBuffer: public IRhiBuffer
+{
+  public:
+	virtual ~IRhiHostBuffer() = default;
+
+	virtual IRhiMemory* GetMemory() = 0;
+	virtual ssize GetSize() = 0;
+
+	/// Map the buffer into host address space
+	virtual uptr Map(u64 size, u64 baseOffset = 0) = 0;
+	
+	/// Map the buffer as a specific type
+	template <typename T> T* Map(u64 size, u64 baseOffset = 0)
+	{
+		return reinterpret_cast<T*>(Map(size, baseOffset));
+	}
+
+	/// Unmap the buffer
+	virtual void Unmap(uptr hostAddr, u64 size) = 0;
+};
